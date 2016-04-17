a1245429160461c69e262a5bb02c39663e18c2f0
yangyang861115 <yangyang861115@gmail.com>
2016-04-17 19:56:50 -0400

    fix but for trangle and navbar

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Main AngularJS Web Application
  */
 (function () {
-    var app = angular.module('MYLCApp', ['ngRoute']);
+    var app = angular.module('MYLCApp', ['ngRoute', 'ui.bootstrap']);
 
     /**
      * Configure the Routes
@@ -15,7 +15,7 @@
                 templateUrl: "partials/home.html"
             })
             .when("/about", {
-                templateUrl: "partials/about.html"
+                templateUrl: "partials/about.html",
             })
             .when("/resources", {
                 templateUrl: "partials/resources.html"
@@ -30,63 +30,111 @@
                 redictTo: "/"
             });
     }
+    /**
+     * Controls the page
+     */
+
+    app.controller('PageCtrl', pageCtrl);
+
+    function pageCtrl($scope, $location){
+
+        //show or hide the jumbotron
+        $scope.checkURl = function() {
+            if($location.url() == '/' || $location.url().indexOf('/#') > -1) return true;
+            return false;
+        }
+
+        //accordion
+        $scope.status = {
+            isFirstOpen: true,
+            isSecondOpen: false,
+            isThirdOpen: false
+        };
+
+        $scope.load = function() {
+            // do your $() stuff here
+
+            $('#mainNav').affix({
+                offset: {
+                    top: 100
+                }
+            });
+
+            $('#formvals').formValidation({
+                framework: 'bootstrap',
+                icon: {
+                    valid: 'glyphicon glyphicon-ok',
+                    invalid: 'glyphicon glyphicon-remove',
+                    validating: 'glyphicon glyphicon-refresh'
+                },
+                fields: {
+                    nameline: {
+                        verbose:false,
+                        icon: false,
+                        trigger: 'blur',
+                        validators: {
+                            notEmpty: {
+                                message: 'Your name is required'
+                            }
+                        }
+                    },
+                    email: {
+                        verbose:false,
+                        icon: false,
+                        trigger: 'blur',
+                        validators: {
+                            notEmpty: {
+                                message: 'The email is required and cannot be empty'
+                            },
+                            emailAddress: {
+                                message: 'The value is not a standard email address'
+                            },
+                            regexp: {
+                                regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
+                                message: 'The value is not a valid email address'
+                            }
+                        }
+                    }
+                }
+            });
+            $('#frmsbmt').on("click",function(e){
+                $("#formvals").data('formValidation').validate();
+                if(!$("#formvals").data('formValidation').isValid()) {
+                    return false; }
+                e = e || window.event;
+                e.preventDefault();
+                $.ajax({
+                    url:"https://crucore.com/api.php",
+                    data:$("#formvals").serialize(),
+                    dataType:"JSON",
+                    type:"POST",
+                    success: function(data){
+                        if(data.success) {
+                            var msg = data.msg;
+                            $("#showresp").html(msg);
+                            // window.location="http://essentials24.org";
+                        }
+                        else {
+                            alert("You are at Failure");
+                            var ply = new Ply({el:data.error});
+                            ply.open();
+                        }
+                    }
+                });
+            });
+
+        };
+
+        //don't forget to call the load function
+        $scope.load();
+
+
+    }
+
 })();
 
 
 
-
-//
-//
-//app.config(['$routeProvider', function ($routeProvider) {
-//    $routeProvider
-//    // Home
-//        .when("/", {
-//            templateUrl: "partials/home.html",
-//            controller: "PageCtrl"
-//        })
-//        // Pages
-//        .when("/about", {
-//            templateUrl: "partials/about.html",
-//            controller: "PageCtrl"
-//        })
-//        .when("/faq", {
-//            templateUrl: "partials/faq.html",
-//            controller: "PageCtrl"
-//        })
-//        .when("/pricing", {
-//            templateUrl: "partials/pricing.html",
-//            controller: "PageCtrl"
-//        })
-//        .when("/services", {
-//            templateUrl: "partials/services.html",
-//            controller: "PageCtrl"
-//        })
-//        .when("/contact", {
-//            templateUrl: "partials/contact.html",
-//            controller: "PageCtrl"
-//        })
-//        // Blog
-//        .when("/blog", {
-//            templateUrl: "partials/blog.html",
-//            controller: "BlogCtrl"
-//        })
-//        .when("/blog/post", {
-//            templateUrl: "partials/blog_item.html",
-//            controller: "BlogCtrl"
-//        })
-//        // else 404
-//        .otherwise("/404", {
-//            templateUrl: "partials/404.html",
-//            controller: "PageCtrl"
-//        });
-//}]);
-//
-///**
-// * Controls the Blog
-// */
-//app.controller('BlogCtrl', function ( /* $scope, $location, $http */ ) {
-//    console.log("Blog Controller reporting for duty.");
-//});
 //
 ///**
 // * Controls all other Pages
--- js/diagonal.js
+++ js/diagonal.js
@@ -6,7 +6,7 @@ window.onresize = function(event) {
 function set_diagonals(){
   var width = $('.s_1').width();
   var height = width * 0.05;
-  //console.log(width, height);
+  console.log(width, height);
   $('.diagonal').css('border-left-width', width + 100);
   $('.diagonal_top').css('border-top-width', height);
   $('.diagonal_bottom').css('border-bottom-width', height);
